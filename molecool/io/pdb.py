05c0efdca6ebef77445c867f3b64d67e84624c23
Max Novelli <man8@pitt.edu>
2019-11-07 15:32:08 -0500

    Adopted PEP8 code style

--- molecool/io/pdb.py
+++ molecool/io/pdb.py
@@ -11,18 +11,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def open_pdb(f_loc):
+def open_pdb(file_location):
     # This function reads in a pdb file and returns the atom names and coordinates.
-    with open(f_loc) as f:
+    with open(file_location) as f:
         data = f.readlines()
-    c = []
+    coordinates = []
     sym = []
-    for l in data:
-        if 'ATOM' in l[0:6] or 'HETATM' in l[0:6]:
+    for line in data:
+        if 'ATOM' in line[0:6] or 'HETATM' in line[0:6]:
             sym.append(l[76:79].strip())
-            c2 = [float(x) for x in l[30:55].split()]
-            c.append(c2)
-    coords = np.array(c)
+            atom_coords = [float(x) for x in l[30:55].split()]
+            coordinates.append(atom_coords)
+    coords = np.array(coordinates)
     return sym, coords
 
 
